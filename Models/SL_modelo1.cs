7b735d50d6a71e022da2e7c174e5c7bc51c2ac07
mango2309 <sebastian.largo@udla.edu.ec>
2024-04-24 12:07:17 -0500

    Se realizo la pestaña de crear usuario y la modificacion de los datos.

--- Models/SL_modelo1.cs
+++ Models/SL_modelo1.cs
@@ -7,14 +7,14 @@ namespace SebastianLargo_Examen1P.Models
         [Key]
         public int SLPerfilID {  get; set; }
 
-        [Required]
-        public int SLDecimal {  get; set; }
-
-        [Required]
+        [Required(ErrorMessage = "El campo solo debe contener letras")]
         public string? SLNombre { get; set; }
 
         [Required(ErrorMessage ="El caracter debe ser verdadero o falso")]
         public bool SLCaracter {  get; set; }
+        
+        [Required]
+        public int SLDecimal {  get; set; }
 
         [Required]
         public DateTime Fecha {  get; set; }
